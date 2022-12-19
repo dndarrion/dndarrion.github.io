e1c6ae3dd75580fd4f91e8c438ddb8278710502f
dndarrion <darrionnongbet404@gmail.com>
2022-12-19 23:16:26 +0530

    cleaned up the about secion & added highlights for selected section

--- files/script.js
+++ files/script.js
@@ -6,11 +6,15 @@ let aboutPanel = document.getElementsByClassName("about")[0];
 
 const showAbout = () => {
   linksPanel.style.display = "none";
+  linkButton.style.color = "#C8C8C8";
   aboutPanel.style.display = "block";
+  aboutButton.style.color = "#FF00BB";
 };
 const showLinks = () => {
   linksPanel.style.display = "block";
+  linkButton.style.color = "#FF00BB";
   aboutPanel.style.display = "none";
+  aboutButton.style.color = "#C8C8C8";
 };
 linkButton.addEventListener("click", showLinks);
 aboutButton.addEventListener("click", showAbout);
